efbf8200fb1621dc58e8f8069aae7c60329b3918
HovSkay <32835549+Hovsky@users.noreply.github.com>
2019-12-09 00:29:38 +0100

    all functionalities implemented, need to sweeten the look

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
 //many global variables, will solve later
-var rngArray = new Array(); rngArray = [8];
-var rngDiv = new Array(); rngDiv = [8];
+var rngArray = new Array();
+rngArray = [8];
+var rngDiv = new Array();
+rngDiv = [8];
 
-var playDiv = new Array(); playDiv = [24];
-var playArray = [0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6]
+var playDiv = new Array();
+playDiv = [24];
+var playArray = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6]
+
+var checkArray = [playArray[6], playArray[7], playArray[8], playArray[11], playArray[12], playArray[13], playArray[16], playArray[17], playArray[18]];
 
 var colors = ["red", "blue", "white", "orange", "yellow", "green", "black"];
-
-//initializing divs into arrays
+var playTable = document.getElementById("player_table");
+//initializing div into arrays
 window.onload = function () {
     for (let i = 0; i < 9; i++) {
         rngDiv[i] = document.getElementById("r" + i);
@@ -20,9 +25,10 @@ window.onload = function () {
 function RNG(c) {
     return Math.floor((Math.random() * c));
 }
-//shuffles elements of array in the same array, so i don't have to have more array wooo XD
+//shuffles elements of array in the same array, so i don't have to have more array XD
 function shuffleColor(arr) {
-    let m = arr.length, t1, t2;
+    let m = arr.length,
+        t1, t2;
     while (m) {
         t1 = RNG(m--);
         t2 = arr[m];
@@ -39,7 +45,7 @@ function testDuplicate(A) {
     for (let i = 0; i < A.length; i++) {
         testArray[i] = 0;
     }
-    //always small arrays, can have quadratic time wooo
+    //always small arrays, can have quadratic time
     for (let i = 0; i < A.length; i++) {
         let testHold = 0;
         for (let j = 0; j < A.length; j++) {
@@ -53,13 +59,13 @@ function testDuplicate(A) {
 
         } else {
             return 0;
-        } 
+        }
     }
 }
 
 // check condition playDiv [6,7,8,11,12,13,16,17,18]
 function testCondition(playBoard, testBoard) {
-    //check manually playboard[6,7,8,11,12,13,16,17,18] against testboard[0,1,2,3,4,5,6,7,8]
+    //check manually playBoard[6,7,8,11,12,13,16,17,18] against testBoard[0,1,2,3,4,5,6,7,8]
 }
 
 //self explanatory
@@ -70,17 +76,99 @@ function restart() {
     testDuplicate(rngArray);
     for (let i = 0; i < 9; i++) {
         rngDiv[i].style.backgroundColor = colors[rngArray[i]];
-    } 
+    }
 
     shuffleColor(playArray);
 
-    for (let i = 0; i< 25; i++) {
+    for (let i = 0; i < 25; i++) {
         playDiv[i].style.backgroundColor = colors[playArray[i]];
     }
 }
 
-document.addEventListener("click", changeTile);
+function checkCond(play, cond) {
+    if (play == cond) {
+        return 1;
+    }
+}
 
-function changeTile(e) {
-    console.log(e.target.style.backgroundColor);
+playTable.addEventListener("click", function (e) {
+    if (e.target.style.backgroundColor != "black") {
+        let a = parseInt(e.path[0].id.substr(1), 10);
+        let t = 0;
+        if (playArray[a - 5] == 6) {
+            t = playArray[a - 5];
+            playArray[a - 5] = playArray[a];
+            playArray[a] = t;
+        }
+        if (playArray[a - 1] == 6) {
+            t = playArray[a - 1];
+            playArray[a - 1] = playArray[a];
+            playArray[a] = t;
+        }
+        if (playArray[a + 1] == 6) {
+            t = playArray[a + 1];
+            playArray[a + 1] = playArray[a];
+            playArray[a] = t;
+        }
+        if (playArray[a + 5] == 6) {
+            t = playArray[a + 5];
+            playArray[a + 5] = playArray[a];
+            playArray[a] = t;
+        }
+        //bruteforce rendering whole array since it isn't big array
+        for (let i = 0; i < 25; i++) {
+            playDiv[i].style.backgroundColor = colors[playArray[i]];
+        }
+        checkArray = [playArray[6], playArray[7], playArray[8], playArray[11], playArray[12], playArray[13], playArray[16], playArray[17], playArray[18]];
+
+        if (JSON.stringify(checkArray) === JSON.stringify(rngArray)) {
+            console.log("POBJEDA!");
+        }
+    }
+});
+
+document.onkeydown = keyNav;
+
+function keyNav(e) {
+    let p = playArray.indexOf(6);
+    //gore
+    if (e.keyCode == 38) {
+        if (p > 4) {
+            let t = playArray[p - 5];
+            playArray[p - 5] = playArray[p];
+            playArray[p] = t;
+        }
+    }
+    //dolje
+    if (e.keyCode == 40) {
+        if (p < 20) {
+            let t = playArray[p + 5];
+            playArray[p + 5] = playArray[p];
+            playArray[p] = t;
+        }
+    }
+    //lijevo
+    if (e.keyCode == 37) {
+        if (p != 0 && p != 5 && p != 10 && p != 15 && p != 20) {
+            let t = playArray[p - 1];
+            playArray[p - 1] = playArray[p];
+            playArray[p] = t;
+        }
+    }
+    //desno
+    if (e.keyCode == 39) {
+        if (p != 4 && p != 9 && p != 14 && p != 19 && p != 24) {
+            let t = playArray[p + 1];
+            playArray[p + 1] = playArray[p];
+            playArray[p] = t;
+        }
+    }
+    for (let i = 0; i < 25; i++) {
+        playDiv[i].style.backgroundColor = colors[playArray[i]];
+    }
+    checkArray = [playArray[6], playArray[7], playArray[8], playArray[11], playArray[12], playArray[13], playArray[16], playArray[17], playArray[18]];
+
+    if (JSON.stringify(checkArray) === JSON.stringify(rngArray)) {
+        console.log("POBJEDA!");
+    }
 }
